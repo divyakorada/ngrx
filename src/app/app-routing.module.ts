1ab71540904197ca366255ceb066389648852e1c
divyakorada <sreedivvu@gmail.com>
2024-04-19 16:18:33 +0530

    15. Create Reactive Form for adding Post and apply validations using child routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { CounterComponent } from "./counter/counter/counter.component";
 import { PostsListComponent } from "./posts/posts-list/posts-list.component";
+import { AddPostComponent } from "./posts/add-post/add-post.component";
 
 const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'counter', component: CounterComponent},
-    {path: 'posts', component: PostsListComponent}
+    {path: 'posts', component: PostsListComponent, children: [
+        {path: 'add', component: AddPostComponent}
+    ]}
 ]
 
 @NgModule({
