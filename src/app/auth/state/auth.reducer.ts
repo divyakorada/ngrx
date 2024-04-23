d7dd8a24a24243bff6d5c227dbc19bafcd04ef28
divyakorada <sreedivvu@gmail.com>
2024-04-23 12:42:25 +0530

    27. Save the User token data from Login in Ngrx State in Angular Ngrx Application

--- src/app/auth/state/auth.reducer.ts
+++ src/app/auth/state/auth.reducer.ts
@@ -1,7 +1,18 @@
-import { createReducer } from "@ngrx/store"
+import { createReducer, on } from "@ngrx/store"
 import { initialState } from "./auth.state"
+import { loginSuccess } from "./auth.actions";
+import { state } from "@angular/animations";
 
-const _authReducer = createReducer(initialState);
+const _authReducer = createReducer(initialState, 
+    on(loginSuccess, (state, action) => {
+        console.log('state', state);
+        console.log('action', action);
+        return {
+            ...state,
+            user: action.userInfo
+        }
+    })
+);
 
 export function AuthReducer(state: any, action: any) {
     return _authReducer(state, action)
